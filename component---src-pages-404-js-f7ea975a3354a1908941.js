6e4afa1ba8be26c6033f749e5d00689734d2f0ca
Myron Mavko <myron@flexum.io>
2021-02-19 20:10:44 +0200

    Initial commit

---
+++ component---src-pages-404-js-f7ea975a3354a1908941.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{w2l6:function(n,e,t){"use strict";t.r(e);var a=t("q1tI"),o=t.n(a),u=t("7oih");e.default=function(){return o.a.createElement(u.a,null,o.a.createElement("p",null,"404 not found"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-f7ea975a3354a1908941.js.map
